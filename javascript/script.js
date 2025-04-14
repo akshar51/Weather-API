2da43c59d2f122a09e6dff8b8572047b758f766d
akshar51 <aksharparekh401@gmail.com>
2025-04-14 13:09:12 +0530

    done

--- javascript/script.js
+++ javascript/script.js
@@ -1,27 +1,47 @@
 const apiKey = '67ac3361a81823463669ff6f146ab856'; 
-const city = 'England';
+let searchBtn = document.getElementById("search")
+let inputData = document.getElementById("inputData");
+inputData.focus();
 
-let place = document.querySelector("location")
-
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
-  .then(response => response.json())
-  .then(data => {
+searchBtn.addEventListener("click",async ()=>{
+    const city = document.getElementById("inputData").value.trim();
+    let url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+    let data = await url.json();
     console.log(data)
+    document.querySelector(".temperature").textContent = Math.round(data.main.temp)+`°`;
+    document.querySelector(".weather").textContent = data.weather[0].main
+    document.querySelector(".location").innerHTML = `<i class="bi bi-geo-alt-fill"></i> ${data.name}, ${data.sys.country}`
+    document.querySelector(".feel-temp").innerHTML = Math.round(data.main.feels_like);
+    document.querySelector(".humidity-percentage").innerHTML = `${data.main.humidity}%`;
+    document.getElementById("weather-img").src = getWeatherIcon(data.weather[0].main);
+    document.querySelector(".weather-data").classList.remove("active");
+    document.querySelector(".weather-card").classList.add("active");
+    
+})
 
-   document.querySelector(".location").innerHTML = data.name
-   document.querySelector(".temp").innerHTML = data.main.temp
-   document.querySelector(".condition").innerHTML = data.weather[0].description
-   document.querySelector(".humidity span").innerHTML = `${data.main.humidity}%`
-   document.querySelector(".wind span").innerHTML = `${data.wind.speed} m/s`
-   document.querySelector(".feel span").innerHTML = `${data.main.feels_like} C`
+function getWeatherIcon(condition){
+  switch (condition.toLowerCase()) {
+    case "clear":
+      return "./image/clear.svg";
+    case "clouds":
+      return "./image/cloud.svg";
+    case "rain":
+      return "./image/rain.svg";
+    case "snow":
+      return "./image/snow.svg";
+    case "drizzle":
+      return "./image/drizzle.svg";
+    case "thunderstorm":
+      return "./image/storm.svg";
+    default:
+      return "./image/clear.svg";
+  }
+}
 
-   
-
-    // const weatherDiv = document.getElementById('weather');
-    // weatherDiv.innerHTML = `
-    //   <p>City: ${data.name}</p>
-    //   <p>Temperature: ${data.main.temp}°C</p>
-    //   <p>Weather: ${data.weather[0].description}</p>
-    // `;
-  })
-  .catch(error => console.error('Error fetching weather data:', error));
+let backBtn = document.querySelector(".bi-arrow-left-short");
+backBtn.addEventListener("click",()=>{
+  document.querySelector(".weather-data").classList.add("active");
+  document.querySelector(".weather-card").classList.remove("active");
+  inputData.value = "";
+  inputData.focus();
+})
